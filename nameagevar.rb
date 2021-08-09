142c00771aaba83dae9977b91af1f5c4ec7468f6
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-09 19:48:56 +0530

    Add files via upload

---
+++ nameagevar.rb
@@ -0,0 +1,8 @@
+character_name="George"
+character_age="35"
+puts(+character_name+ "is a great man")
+puts("he's " +character_age+ " years old")
+character_name1="tom"
+puts( +character_name+"liked his name")
+character_age1="70"
+puts("but he didnt wanted to be "+character_age1+ " but more younger")
