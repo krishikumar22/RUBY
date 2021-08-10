1dd35f6b4e61b58ce2c8f4f7d8a5394216db2936
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-10 07:51:54 +0530

    Add files via upload

---
+++ MADSLIBS.rb
@@ -0,0 +1,13 @@
+puts "enter the name of flower"
+Flowers=gets.chomp()
+
+puts "enter the name of color"
+color=gets.chomp()
+
+puts "enter the name of celebrity"
+celebrity=gets.chomp()
+
+
+puts(+Flowers+ "are red")
+puts(+color+ "are blue")
+puts("i love" +celebrity)
