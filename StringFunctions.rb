4df837fd5bc377dd049faa64d1af3a903c0353ad
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-10 07:13:06 +0530

    Add files via upload

---
+++ StringFunctions.rb
@@ -0,0 +1,25 @@
+phrase="  pro coder  "
+puts phrase
+print phrase
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.strip()
+puts phrase.length()
+puts phrase.include?"pro"
+puts phrase.include?"con"
+puts phrase[4]
+puts phrase[0,8]
+puts phrase.index("d")
+puts phrase.index("r")
+
+print "  pro coder  "
+puts "  pro coder  ".upcase()
+puts "  pro coder  ".downcase()
+puts "  pro coder  ".strip()
+puts "  pro coder  ".length()
+puts "  pro coder  ".include?"pro"
+puts "  pro coder  ".include?"con"
+puts "  pro coder  "[4]
+puts "  pro coder  "[0,8]
+puts "  pro coder  ".index("d")
+puts "  pro coder  ".index("r")
