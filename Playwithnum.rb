eb0826cff373b5219e8506291145b7846baaed19
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-10 07:28:00 +0530

    Add files via upload

---
+++ Playwithnum.rb
@@ -0,0 +1,13 @@
+num=7
+puts num.abs()
+puts num.ceil
+puts Math.log(num)
+puts Math.sqrt(36)
+puts("my fav number is " +num.to_s)
+num2=19
+puts num+num2
+puts num-num2
+puts num*num2
+puts num2/num
+puts num**num2
+puts num2%num
